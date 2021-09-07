93b8e45c82ecf12cd71e5c7a88f10f06df8f4fb9
Livingstone Mumelo <lmumelo@kemri-wellcome.org>
2021-09-07 20:23:30 +0300

    First commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+        <div>
+            <h1>Gari safi</h1>
+            <h1>Your number one Car autoparts destination</h1>
+        </div>
+        
+        )
+}
+export default Header;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+
+const Header = () => {
+
+
+return(
+	<div>
+        <h1>Gari safi</h1>
+        <h3>Your Number one Autoparts shop</h3>
+    </div>
+    )
+}
+    export default Header;
